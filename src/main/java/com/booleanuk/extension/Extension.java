420e23a6fcb875a17b30ee2ee645a387ba2797c5
Dave Ames <david.john.ames@gmail.com>
2023-03-30 10:16:05 +0100

    Added solutions for the Extension Exercise.

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -6,20 +6,23 @@ public class Extension extends ExtensionBase {
     // Let's make a cake!
 
     /*
-        5. Create a method named bakingTime that returns the number 50
+        5. Create a method named bakingTime that outputs the number 50
      */
-
-
+    public int bakingTime() {
+        return 50;
+    }
 
 
     /*
         6. Create a method named remainingBakeTime that accepts one input:
         - the number of minutes the cake has been in the oven
 
-        It must return how many minutes are left to bake based on the input
+        It must output how many minutes are left to bake based on the input
         and the result of calling the bakingTime method
      */
-
+    public int remainingBakeTime(int timeInOven) {
+        return bakingTime() - timeInOven;
+    }
 
 
 
@@ -27,11 +30,13 @@ public class Extension extends ExtensionBase {
         7. Create a method named calculatePrepTime that accepts one input:
         - the number of layers the cake has
 
-        It must return how many minutes it will take to prepare the cake based on
+        It must output how many minutes it will take to prepare the cake based on
         each layer taking 3 minutes to prepare
      */
 
-
+    public int calculatePrepTime(int numberOfLayers) {
+        return 3*numberOfLayers;
+    }
 
 
     /*
@@ -39,12 +44,13 @@ public class Extension extends ExtensionBase {
         - the number of layers the cake has
         - the number of minutes the cake has already been in the oven
 
-        It must return how many minutes in total you have spent making the cake,
+        It must output how many minutes in total you have spent making the cake,
         which is the sum of the preparation time and the number of minutes it's been
         in the oven. Use your calculatePrepTime method in the calculation
      */
+    public int totalTimeSpent(int layers, int inOven) {
 
-
-
+        return calculatePrepTime(layers) + inOven;
+    }
 
 }
